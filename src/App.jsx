6c0d7557fccf2283e3017111f348782b6c23b8ad
zelkovaria <hyein396@gmail.com>
2024-01-14 03:32:01 +0900

    섹션3-42 이미지 파일 import로 속성 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import reactImg from "./assets/react-core-concepts.png";
+
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function getRandomInt(max) {
@@ -9,7 +11,7 @@ function Hedaer() {
 
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
         {description} React concepts you will need for almost any app you are
