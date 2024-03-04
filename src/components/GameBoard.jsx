47432542ea6815e204c62b06239b65cb2ec29949
zelkovaria <hyein396@gmail.com>
2024-03-05 00:05:08 +0900

    3x3 형태의 버튼 게임판 구현 - 섹션4-79

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+const initialGameBoard = [
+  // ["x", "x", "x"],
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 const GameBoard = () => {
-  return <div></div>;
+  return (
+    <ol id="game-board">
+      {initialGameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((col, colIndex) => (
+              <li key={colIndex}>
+                <button>{col}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
 };
 
 export default GameBoard;
